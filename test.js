b24eabfe532dcad6d2ba9ddd079846edafe060e4
gyeongyeon-jeon <gy.jeon@purple.io>
2022-06-30 11:18:45 +0900

    toThrow

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
-// function getUser(id) {
-//   return {
-//     id,
-//     email: `user${id}@test.com`,
-//   };
-// }
+function getUser(id) {
+  if (id <= 0) throw new Error("Invalid ID");
+  return {
+    id,
+    email: `user${id}@test.com`,
+  };
+}
 
-test("string", () => {
-  expect(getUser(1).email).toBe("user1@test.com");
-  expect(getUser(2).email).toMatch(/.*test.com$/);
+test("throw when id is non negative", () => {
+  expect(() => getUser(-1)).toThrow();
+  expect(() => getUser(-1)).toThrow("Invalid ID");
 });
+/**
+ * 검증 대상을 함수로 한 번 감싸줘야 한다.
+ * 그렇지 않으면 예외 발생 여부를 체크하는 것이 아니라,
+ * 실행 도중 예외가 발생하기 때문에 그 테스트는 항사 실패한다.
+ */
