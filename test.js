ee7e79fe5c976063af2cd94da8059a9e3b0c40cb
gyeongyeon-jeon <gy.jeon@purple.io>
2022-06-30 11:17:15 +0900

    toHaveLength, toContain

--- test.js
+++ test.js
@@ -5,7 +5,9 @@
 //   };
 // }
 
-test("number 0 is falsy but string 0 is truthy", () => {
-  expect(0).toBeFalsy();
-  expect("0").toBeTruthy();
+test("array", () => {
+  const colors = ["Red", "Yellow", "Blue"];
+  expect(colors).toHaveLength(3);
+  expect(colors).toContain("Yellow");
+  expect(colors).not.toContain("Green");
 });
