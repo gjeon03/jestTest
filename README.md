e80abdb5a28df8fbe3476cdbc0670692f33adbe8
gyeongyeon-jeon <gy.jeon@purple.io>
2022-06-30 11:14:40 +0900

    describe, beforeAll, afterAll, beforeEach, afterEach 사용법

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-공부 참고
-https://www.daleseo.com/jest-basic/
+### describe, beforeAll, afterAll, beforeEach, afterEach 사용법
+
+- describe
+
+  안에 있는 모든 테스트를 테스트 하겠다.
+
+- beforeAll
+
+  test들이 모두 다 시작하기 전에 실행되는 부분을 의미
+
+- afterAll
+
+  test들이 모두 실행되고 난 후에 실행되는 부분
+
+- beforeEach
+
+  각 test들이 실행되기 전에 실행되는 부분을 의미
+
+- afterEach
+
+  각 test들이 실행된 이후 실행되는 부분을 의미
+
+#### Reference
+
+- https://www.daleseo.com/jest-basic/
+- https://blog.naver.com/PostView.nhn?blogId=username1103&logNo=222391189854
--- test.js
+++ test.js
@@ -1,3 +1,29 @@
-test("1 is 1", () => {
-  expect(1).toBe(1);
+let var1;
+describe("문법 테스트", () => {
+  beforeAll(() => {
+    var1 = 0;
+    console.log(`beforeAll => var1:${var1}`);
+  });
+  afterAll(() => {
+    var1 = -1;
+    console.log(`afterAll => var1:${var1}`);
+  });
+  beforeEach(() => {
+    console.log(`beforeEach => var1:${var1}`);
+  });
+  afterEach(() => {
+    console.log(`afterEach => var1:${var1}`);
+  });
+  test("문법 예제 test111", () => {
+    var1 = var1 + 1;
+    expect(var1).toBe(1);
+  });
+  test("문법 예제 test222", () => {
+    var1 = var1 + 1;
+    expect(var1).toBe(2);
+  });
+  test("문법 예제 test333", () => {
+    var1 = var1 + 1;
+    expect(var1).toBe(3);
+  });
 });
