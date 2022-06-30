5e3999eea116b1e84378ff85b064ecec5c6142f2
gyeongyeon-jeon <gy.jeon@purple.io>
2022-06-30 11:17:39 +0900

    toMatch

--- test.js
+++ test.js
@@ -5,9 +5,7 @@
 //   };
 // }
 
-test("array", () => {
-  const colors = ["Red", "Yellow", "Blue"];
-  expect(colors).toHaveLength(3);
-  expect(colors).toContain("Yellow");
-  expect(colors).not.toContain("Green");
+test("string", () => {
+  expect(getUser(1).email).toBe("user1@test.com");
+  expect(getUser(2).email).toMatch(/.*test.com$/);
 });
