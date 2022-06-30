861365aa17c877ce63f8e1e91b84f9e75dac992e
gyeongyeon-jeon <gy.jeon@purple.io>
2022-06-30 11:16:11 +0900

    toEqual

--- test.js
+++ test.js
@@ -1,29 +1,13 @@
-let var1;
-describe("문법 테스트", () => {
-  beforeAll(() => {
-    var1 = 0;
-    console.log(`beforeAll => var1:${var1}`);
-  });
-  afterAll(() => {
-    var1 = -1;
-    console.log(`afterAll => var1:${var1}`);
-  });
-  beforeEach(() => {
-    console.log(`beforeEach => var1:${var1}`);
-  });
-  afterEach(() => {
-    console.log(`afterEach => var1:${var1}`);
-  });
-  test("문법 예제 test111", () => {
-    var1 = var1 + 1;
-    expect(var1).toBe(1);
-  });
-  test("문법 예제 test222", () => {
-    var1 = var1 + 1;
-    expect(var1).toBe(2);
-  });
-  test("문법 예제 test333", () => {
-    var1 = var1 + 1;
-    expect(var1).toBe(3);
+function getUser(id) {
+  return {
+    id,
+    email: `user${id}@test.com`,
+  };
+}
+
+test("return a user object", () => {
+  expect(getUser(1)).toEqual({
+    id: 1,
+    email: `user1@test.com`,
   });
 });
